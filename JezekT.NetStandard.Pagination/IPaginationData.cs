43e2aa307b66da64924f7d6b9d6a2468b44f67c1
Tomáš Ježek <jezek.mail@seznam.cz>
2017-03-15 11:42:05 +0100

    Add NetStandard.Pagination project, IPaginationData, IPaginationDataProvider

---
+++ JezekT.NetStandard.Pagination/IPaginationData.cs
@@ -0,0 +1,9 @@
+﻿namespace JezekT.NetStandard.Pagination
+{
+    public interface IPaginationData
+    {
+        object[] Items { get; set; }
+        int RecordsTotal { get; set; }
+        int RecordsFiltered { get; set; }
+    }
+}
---
+++ JezekT.NetStandard.Pagination/IPaginationDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using JezekT.NetStandard.Data;
+
+namespace JezekT.NetStandard.Pagination
+{
+    public interface IPaginationDataProvider
+    {
+        Task<IPaginationData> GetPaginationDataAsync(string term, int start, int pageSize, string orderField, string orderDirection);
+    }
+
+    public interface IPaginationDataProvider<TEntity, in TId> where TEntity : class, IWithId<TId>
+    {
+        Task<IPaginationData> GetPaginationDataAsync(string term, int start, int pageSize, string orderField, string orderDirection, TId[] inputFiletrIds = null);
+    }
+
+}
