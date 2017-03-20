5d9bcade5f9baa9593c8ab36d09ef2521d8e125d
Tomáš Ježek <jezek.mail@seznam.cz>
2017-03-20 15:27:48 +0100

    Add class PaginationResponse, edit interface IPaginationDataProvider

--- JezekT.NetStandard.Pagination/DataProviders/IPaginationDataProvider.cs
+++ JezekT.NetStandard.Pagination/DataProviders/IPaginationDataProvider.cs
@@ -1,16 +1,21 @@
 ﻿using System.Threading.Tasks;
 using JezekT.NetStandard.Data;
 
-namespace JezekT.NetStandard.Pagination
+namespace JezekT.NetStandard.Pagination.DataProviders
 {
     public interface IPaginationDataProvider
     {
         Task<IPaginationData> GetPaginationDataAsync(string term, int start, int pageSize, string orderField, string orderDirection);
     }
 
-    public interface IPaginationDataProvider<TEntity, in TId> where TEntity : class, IWithId<TId>
+    public interface IPaginationDataProvider<TEntity, in TId> : IPaginationDataProvider<TId> where TEntity : class, IWithId<TId>
+    {
+    }
+
+    public interface IPaginationDataProvider<in TId>
     {
         Task<IPaginationData> GetPaginationDataAsync(string term, int start, int pageSize, string orderField, string orderDirection, TId[] inputFiletrIds = null);
     }
+
 
 }
---
+++ JezekT.NetStandard.Pagination/PaginationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace JezekT.NetStandard.Pagination
+{
+    public class PaginationResponse : IPaginationData
+    {
+        public object[] Items { get; set; }
+        public int RecordsTotal { get; set; }
+        public int RecordsFiltered { get; set; }
+    }
+}
