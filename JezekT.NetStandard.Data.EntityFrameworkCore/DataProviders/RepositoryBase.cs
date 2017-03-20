9f8a6812476dc797b1a9073a02205d1590f849ad
Tomáš Ježek <jezek.mail@seznam.cz>
2017-03-20 15:26:27 +0100

    Add project Data.EntityFrameworkCore, classes RepositoryBase and UnityOfWork

---
+++ JezekT.NetStandard.Data.EntityFrameworkCore/DataProviders/RepositoryBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace JezekT.NetStandard.Data.EntityFrameworkCore.DataProviders
+{
+    public abstract class RepositoryBase<TEntity, TId, TContext> 
+        where TEntity : class, IWithId<TId>
+        where TContext : DbContext
+    {
+        protected TContext DbContext { get; }
+
+
+        public virtual async Task<TEntity> GetByIdAsync(TId id)
+        {
+            return await DbContext.Set<TEntity>().FindAsync(id);
+        }
+        
+        public virtual void Create(TEntity obj)
+        {
+            if (obj == null) throw new ArgumentNullException();
+            Contract.EndContractBlock();
+
+            DbContext.Set<TEntity>().Add(obj);
+        }
+
+        public virtual void Update(TEntity obj)
+        {
+            if (obj == null) throw new ArgumentNullException();
+            Contract.EndContractBlock();
+
+            DbContext.Attach(obj);
+            var entry = DbContext.Entry(obj);
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual void DeleteById(TId id)
+        {
+            var objToRemove = DbContext.Set<TEntity>().Find(id);
+            if (objToRemove == null) throw new InvalidOperationException();
+            DbContext.Set<TEntity>().Remove(objToRemove);
+        }
+
+
+        protected RepositoryBase(TContext dbContext)
+        {
+            if (dbContext == null) throw new ArgumentNullException();
+            Contract.EndContractBlock();
+
+            DbContext = dbContext;
+        }
+
+    }
+}
---
+++ JezekT.NetStandard.Data.EntityFrameworkCore/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using JezekT.NetStandard.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace JezekT.NetStandard.Data.EntityFrameworkCore
+{
+    public class UnitOfWork<TContext> : IUnitOfWork
+        where TContext : DbContext
+    {
+        private readonly TContext _dbContext;
+
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+        public UnitOfWork(TContext dbContext)
+        {
+            if (dbContext == null) throw new ArgumentNullException();
+            Contract.EndContractBlock();
+
+            _dbContext = dbContext;
+        }
+    }
+}
