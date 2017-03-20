e4edc813a72051321434d710fe979ab31991963c
Tomáš Ježek <jezek.mail@seznam.cz>
2017-03-20 15:25:26 +0100

    Add project Data, interfaces IRepository and IUnitOfWork

---
+++ JezekT.NetStandard.Data/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace JezekT.NetStandard.Data.Repository
+{
+    public interface IRepository<T> where T : class 
+    {
+        Task<T> GetByIdAsync(int id);
+        void Create(T jednotka);
+        void Update(T jednotka);
+        void DeleteById(int id);
+    }
+}
---
+++ JezekT.NetStandard.Data/Repository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace JezekT.NetStandard.Data.Repository
+{
+    public interface IUnitOfWork
+    {
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
