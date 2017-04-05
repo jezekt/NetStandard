2ea624d9c585bc6b4e0069b0ded3eadf91f20a97
Tomáš Ježek <jezek.mail@seznam.cz>
2017-04-05 10:51:03 +0200

    Pagination - přidáno ArrayOfTExtensions

---
+++ JezekT.NetStandard.Pagination/Extensions/ArrayOfTExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace JezekT.NetStandard.Pagination.Extensions
+{
+    public static class ArrayOfTExtensions
+    {
+        public static string ToIdsString<TId>(this TId[] ids)
+        {
+            if (ids != null)
+            {
+                if (ids.Any())
+                {
+                    return string.Join(",", ids);
+                }
+                return "*";
+            }
+            return null;
+        }
+    }
+}
